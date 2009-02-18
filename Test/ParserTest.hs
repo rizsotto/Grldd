14c4746e134d2853301578ff804d1aea4aa5ccac
rizsotto <rizsotto@gmail.com>
2009-02-18 21:27:14 +0000

    update tests

--- Test/ParserTest.hs
+++ Test/ParserTest.hs
@@ -1,6 +1,5 @@
 module ParserTest (tests) where
 
-import Types
 import Ldd.Parser
 
 import Text.Parsec.Error
@@ -9,70 +8,63 @@ import Control.Monad (unless)
 import Test.HUnit
 
 
-assertEmpty :: Either ParseError [SoInfo] -> Assertion
+tsrc :: String
+tsrc = "test source"
+
+assertEmpty :: Either ParseError [FilePath] -> Assertion
 assertEmpty value = case value of
-    Right [x] -> unless (isEmpty x) (assertFailure $ message x)
+    Right [x] -> unless (null x) (assertFailure $ message x)
     Right _ -> assertFailure "Expected one empty, but got more than one"
     Left _  -> assertFailure "Unexpected parse error\n"
   where
     message x = "Expected empty, but got: "++ show x ++ "\n"
 
 
-assertSoInfo :: [SoInfo] -> Either ParseError [SoInfo] -> Assertion
+assertSoInfo :: [FilePath] -> Either ParseError [FilePath] -> Assertion
 assertSoInfo expected actual = case actual of
     Right x -> unless (x == expected) (assertFailure $ message x)
     Left _  -> assertFailure "Unexpected parse error\n"
   where
     message x = "Expected: " ++ show expected ++ ", but got: "++ show x ++ "\n"
 
-
-
 emptyTest :: Test
 emptyTest = TestCase (assertEmpty result)
     where
-        result   = parseLdd "" ""
+        result   = parseLdd tsrc ""
 
 staticTest :: Test
 staticTest = TestCase (assertEmpty result)
     where
-        result   = parseLdd "" "\tstatically linked"
+        result   = parseLdd tsrc "\tstatically linked"
 
 normalTest :: Test
 normalTest = TestCase (assertSoInfo expected result)
     where
-        expected = [(name, path)]
-        result   = parseLdd "" ("\t" ++ name ++ " => " ++ path ++ " (0xaa)")
-        name = "liba.so.7"
-        path = "/path/" ++ name
+        expected = ["/path/liba.so.7"]
+        result   = parseLdd tsrc ("\tliba.so.7 => /path/liba.so.7 (0xaa)")
 
 pathTest :: Test
 pathTest = TestCase (assertEmpty result)
     where
-        result   = parseLdd "" "\t/path/liba.so.9 (0xaa)"
+        result   = parseLdd tsrc "\t/path/liba.so.9 (0xaa)"
 
 nameTest :: Test
 nameTest = TestCase (assertEmpty result)
     where
-        result   = parseLdd "" "\tliba.so.8 => (0xaa)"
+        result   = parseLdd tsrc "\tliba.so.8 => (0xaa)"
 
 realTest :: Test
 realTest = TestCase (assertSoInfo expected result)
     where
-        expected = [ empty,
-                    ("libdl.so.2", "/lib/libdl.so.2"),
-                    ("libc.so.6", "/lib/libc.so.6"),
-                     empty ]
-        result   = parseLdd ""
+        expected = ["",
+                    "/lib/libdl.so.2",
+                    "/lib/libc.so.6",
+                    "" ]
+        result   = parseLdd tsrc
                     ("\tlinux-gate.so.1 =>  (0xb7f17000)\n" ++
                      "\tlibdl.so.2 => /lib/libdl.so.2 (0xb7ef4000)\n" ++
                      "\tlibc.so.6 => /lib/libc.so.6 (0xb7d96000)\n" ++
                      "\t/lib/ld-linux.so.2 (0xb7f18000)")
-
-filenameTest :: Test
-filenameTest = TestCase (assertEqual "basename" expected result)
-    where
-        expected = "libpam.so.0"
-        result = parsePath "/lib/libpam.so.0"
 
 tests :: Test
 tests = TestList [ emptyTest
@@ -80,5 +72,4 @@ tests = TestList [ emptyTest
                  , normalTest
                  , pathTest
                  , nameTest
-                 , realTest
-                 , filenameTest ]
+                 , realTest ]
